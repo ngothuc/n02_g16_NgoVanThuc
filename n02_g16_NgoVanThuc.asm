bd0b0a814b2edf53e66db59643dd20b57dc468b5
Ngo Van Thuc <ngothuc0704@gmail.com>
2023-06-30 13:04:57 +0700

    Nhan Lenh Dau Tien

--- n02_g16_NgoVanThuc.asm
+++ n02_g16_NgoVanThuc.asm
@@ -5,17 +5,75 @@
 .eqv WHITE 0x00FFFFFF
 .eqv YELLOW 0x00FFFF00
 .eqv BLACK 0x00000000
+.eqv KEY_CODE 0xFFFF0004 		# ASCII code from keyboard, 1 byte 
+.eqv KEY_READY 0xFFFF0000 		# =1 if has a new keycode ? 
+ 					# Auto clear after lw 
+.eqv DISPLAY_CODE 0xFFFF000C 		# ASCII code to show, 1 byte 
+.eqv DISPLAY_READY 0xFFFF0008 		# =1 if the display has already to do 
+ 					# Auto clear after sw
+.eqv MASK_CAUSE_KEYBOARD 0x0000034 	# Keyboard Cause
 .text
 main:
 	li $k0, MONITOR_SCREEN
 	li $t1, 256
 	li $t2, 256
-	li $s0, 5		#vận tốc
-	jal DrawCircle
-	jal Left
+	li $s0, 10		#vận tốc
+	li $t4, KEY_CODE
+	li $t5, KEY_READY
+loop:
+	nop
+WaitForKey:
+	lw $t8, 0($t5)
+	beq $t8, 0, WaitForKey
+EndWaitKey:
+MakeIntR:
+	teqi $t8, 1
+	j loop	
 endmain:
 	li $v0, 10
 	syscall
+	
+.ktext 0x80000180
+mfc0 $t9, $13
+li $s7, MASK_CAUSE_KEYBOARD
+Keyboard:
+	and $at, $t9, $s7
+	beq $at, $s7, Counter_Keyboard
+	j end_process
+Counter_Keyboard:
+	lw $t0, 0($t4)
+Encrypt:
+	blt $t0, 65, TryToDoSth
+	blt $t0, 91, Lower
+	j TryToDoSth
+	Lower:
+		addi $t0, $t0, 32
+TryToDoSth:
+	beq $t0, 'w', To_Up
+	beq $t0, 's', To_Down
+	beq $t0, 'a', To_Left
+	beq $t0, 'd', To_Right
+	To_Up:	
+		jal Up
+		j end_process
+	To_Down: 
+		jal Down
+		j end_process
+	To_Left:
+		jal Left
+		j end_process
+	To_Right:
+		jal Right
+		j end_process
+	
+end_process:
+next_pc:
+	mfc0 $at, $14 # $at <= Coproc0.$14 = Coproc0.epc 
+ 	addi $at, $at, 4 # $at = $at + 4 (next instruction) 
+ 	mtc0 $at, $14 # Coproc0.$14 = Coproc0.epc <= $at
+return: 
+	eret
+
 DrawCircle:
 	li $t0, YELLOW
 	#Vẽ tâm
